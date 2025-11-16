faf1f3604a64052b4ebbb43cf6be23f5fe9872bf
MuhandisHasan <muhandisarrantisi@gmail.com>
2025-11-16 16:50:06 +0000

    Added select method

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 )
 
 // API response wrapper
-// type Response struct {
-// 	Status string `json:"status"`
-// 	Code   int    `json:"code"`
-// 	Data   []Note `json:"data"`
-// }
+type Response struct {
+	Status string `json:"status"`
+	Code   int    `json:"code"`
+	Data   []Note `json:"data"`
+}
 
 func main() {
 
@@ -19,27 +22,31 @@ func main() {
 		}
 	}()
 
-	Migrate()
+	// note := Note{Title: "First Note", Content: "Hello this is first note"}
+
+	// if err := SaveNotes(&note); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	fmt.Fprintf(w, "Hello World")
 	// })
 
-	// http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/notes", func(w http.ResponseWriter, r *http.Request) {
 
-	// 	notes := GetNotes()
+		notes, _ := GetNotes()
 
-	// 	resp := Response{
-	// 		Status: "success",
-	// 		Code:   200,
-	// 		Data:   notes,
-	// 	}
+		resp := Response{
+			Status: "success",
+			Code:   200,
+			Data:   notes,
+		}
 
-	// 	w.Header().Set("Content-Type", "application/json")
-	// 	json.NewEncoder(w).Encode(resp)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
 
-	// })
+	})
 
-	// log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
--- notes.go
+++ notes.go
@@ -1,19 +1,66 @@
 package main
 
+import "fmt"
+
 type Note struct {
 	ID      int    `json:"id"`
 	Title   string `json:"title"`
 	Content string `json:"content"`
 }
 
-func GetNotes() []Note {
+func GetNotes() ([]Note, error) {
+	db := GetDB()
 
-	table := []Note{
-		{1, "To Do", "- Cleaning"},
-		{2, "Iternary", "- Hagia Sophia"},
-		{3, "Exercise", "- Pushup 25x"},
+	if db == nil {
+		return nil, fmt.Errorf("db is nil")
 	}
 
-	return table
+	query := `SELECT id, title, content FROM notes`
+
+	rows, err := db.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var notes []Note
+
+	for rows.Next() {
+		var note Note
+
+		err := rows.Scan(&note.ID, &note.Title, &note.Content)
+		if err != nil {
+			return nil, err
+		}
+
+		notes = append(notes, note)
+	}
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return notes, nil
+}
+
+func SaveNotes(note *Note) error {
+
+	db := GetDB()
+	if db == nil {
+		return fmt.Errorf("db is nil")
+	}
+
+	query := `INSERT INTO notes (title, content) VALUES (?, ?)`
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(note.Title, note.Content)
+	return err
 
 }
